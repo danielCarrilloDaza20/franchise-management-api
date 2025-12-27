392d10afb141261e251c221f419205131c417690
Daniel Carrillo <daniel.carrillo01@uptc.edu.co>
2025-12-27 00:35:38 -0500

    chore: initial project setup with spring webflux and r2dbc

---
+++ src/test/java/com/company/franchise/franchisemanagementapi/FranchiseManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.franchise.franchisemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
